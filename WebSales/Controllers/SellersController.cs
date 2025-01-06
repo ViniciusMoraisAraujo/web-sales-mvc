ffc89e1430d417a88458b0a306a06787b9aa9ca0
Lonezin <vinidev.morais@gmail.com>
2025-01-06 13:50:49 -0300

    Update seller and custom service exception

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WebSales.Models;
 using WebSales.Models.ViewModels;
 using WebSales.Services;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Controllers
 {
@@ -64,6 +65,36 @@ namespace WebSales.Controllers
 
             return View(obj);
         }
+        public IActionResult Edit(int? id) 
+        {
+            if (id == null) return NotFound();
 
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) return NotFound();
+
+            List<Department> departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller) 
+        {
+            if (id != seller.Id) return BadRequest();
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException) 
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
