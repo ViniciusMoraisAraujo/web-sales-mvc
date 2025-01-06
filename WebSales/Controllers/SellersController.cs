8d4f02738a73f8f57232e1260c7c6534f5399875
Lonezin <vinidev.morais@gmail.com>
2025-01-06 14:34:51 -0300

    created custom error page

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebSales.Models;
@@ -42,10 +43,11 @@ namespace WebSales.Controllers
         }
         public IActionResult Delete(int? id) 
         {
-            if (id == null) return NotFound();
-            
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             var obj = _sellerService.FindById(id.Value);
-            if (obj == null) return NotFound();
+            if (obj == null) 
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
 
             return View(obj);
         }
@@ -58,19 +60,23 @@ namespace WebSales.Controllers
         }
         public IActionResult Details (int? id) 
         {
-            if (id == null) return NotFound();
+            if (id == null) 
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
 
             var obj = _sellerService.FindById(id.Value);
-            if (obj == null) return NotFound();
+            if (obj == null) 
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
 
             return View(obj);
         }
         public IActionResult Edit(int? id) 
         {
-            if (id == null) return NotFound();
+            if (id == null) 
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
 
             var obj = _sellerService.FindById(id.Value);
-            if (obj == null) return NotFound();
+            if (obj == null) 
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
 
             List<Department> departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
@@ -81,20 +87,26 @@ namespace WebSales.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) 
         {
-            if (id != seller.Id) return BadRequest();
+            if (id != seller.Id) return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException) 
+            catch (ApplicationException e) 
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+        }
+
+        public IActionResult Error(string message) 
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
